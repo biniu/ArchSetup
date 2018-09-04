794785f6689a20379aedbeb1630a6863e5a6c0ef
biniu <dawid.binkowski93@gmail.com>
2018-09-04 14:24:14 +0200

    rewrite basic install

--- basic_install.sh
+++ basic_install.sh
@@ -1,100 +1,150 @@
 #!/bin/bash
 
-pacman -Syyu --noconfirm
+source $(dirname "$0")/utils.sh;
 
-echo "" > install.log
-
-function pacman_wrap {
-    echo "Installing [$1]" >> install.log
-    echo "Installing [$1]"
-
-    if (pacman -Q $1 &>/dev/null); then
-        echo "Already installed [$1]"
-        echo "Already installed [$1]" >> install.log
-        echo "***************************" >> install.log
-        return
-    fi
-
-    pacman -S $1 --noconfirm  &>> install.log
-    if [ $? == 0 ]; then
-        echo "Instllation of [$1] OK"
-        echo "***************************" >> install.log
-        echo "Instllation of [$1] OK" >> install.log
-    else
-        echo "Instllation of [$1] ERROR"
-        echo "***************************" >> install.log
-        echo "Instllation of [$1] ERROR" >> install.log
-    fi
+function install_oh_my_zsh {
+    logger "Install oh-my-zsh" 0;
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k;
+    echo "ZSH_THEME=\"powerlevel9k/powerlevel9k\"" >> ~/.zshrc;
 }
 
-function install_oh_my_zsh()
-{
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-    echo "ZSH_THEME=\"powerlevel9k/powerlevel9k\"" >> ~/.zshrc
+function install_vbox {
+    logger "Install Virtual Box" 0;
+    yaourt_wrap virtualbox;
+    yaourt_wrap virtualbox-host-modules-archl
+    yaourt_wrap linux-headers;
+
+    logger "Enable vbox kernel modules" 0;
+    modprobe vboxdrv;
+    modprobe vboxnetadp;
+    modprobe vboxnetflt;
+    modprobe vboxpci;
 }
 
-function install_vbox()
-{
-    pacman_wrap virtualbox
-    pacman_wrap virtualbox-host-modules-arch
-    pacman_wrap linux-headers
+function install_editors {
+    EDITORS_LIST=(
+        'emacs'
+        'vim'
+        'atom'
+        'visual-studio-code-bin'
+        'sublime-text-dev'
+    );
 
-    echo "Enable vbox kernel modules"
-    modprobe vboxdrv
-    modprobe vboxnetadp
-    modprobe vboxnetflt
-    modprobe vboxpci
+    for pkg in "${EDITORS_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+}
+
+function install_cmd_tools {
+    CMD_TOOLS_LIST=(
+        'mc'
+        'tmux'
+        'terminator'
+        'guake'
+        'htop'
+    );
+
+    for pkg in "${CMD_TOOLS_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+}
+
+function install_browers {
+    BROWSERS_LIST=(
+        'firefox'
+        'chromium'
+    );
+
+    for pkg in "${BROWSERS_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+}
+
+function install_basic_dev_tools {
+    BASIC_DEV_TOOLS_LIST=(
+        'git'
+        'zeal'
+        'gitkraken'
+        'git-flow-completion-git'
+    );
+
+    for pkg in "${BASIC_DEV_TOOLS_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+}
+
+function install_cpp_dev_tools {
+    CPP_DEV_LIST=(
+        'gcc'
+        'gdb'
+        'clang'
+        'lld'
+        'llvm'
+        'zapcc'
+        'make'
+        'cmake'
+        'boost'
+        'boost-libs'
+        'cppcheck'
+        'valgrind'
+        'rats'
+        'vera++'
+    );
+
+    for pkg in "${CPP_DEV_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+}
+
+function install_python_dev_tools {
+    PYTHON_DEV_TOOLS=(
+        'python'
+        'python-pip'
+        'python2'
+        'python2-pip'
+    );
+
+    for pkg in "${PYTHON_DEV_TOOLS[@]}"; do
+        yaourt_wrap "$pkg";
+    done
+
+    PIP_MODULES=(
+        'pylint'
+        'django'
+        'virtualenv'
+        'virtualenvwrapper'
+        'flake8'
+        'pyflakes'
+    );
+
+    for pkg in "${PIP_MODULES[@]}"; do
+        pacman_wrap "$pkg";
+    done
+}
+
+function install_other {
+    OTHER_LIST=(
+        'dia'
+        'docker'
+        'docker-compose'
+        'xmind'
+        'arandr'
+        'nemo'
+        'otf-ipafont'    # Japanese fonts
+    );
+
+    for pkg in "${OTHER_LIST[@]}"; do
+        yaourt_wrap "$pkg";
+    done
 }
 
 
-
-install_list=(
-# editors
-    'vim'
-    'emacs'
-
-# CMD tools
-    'mc'
-    'tmux'
-    'terminator'
-    'guake'
-    'htop'
-# Internet
-    'firefox'
-    'chromium'
-    'thunderbird'
-# DEV
-    'python'
-    'python-pip'
-    'python2'
-    'python2-pip'
-    'make'
-    'cmake'
-    'gdb'
-    'gcc'
-    'git'
-    'clang'
-    'clang-tools-extra'
-    'boost'
-    'boost-libs'
-    'cppcheck'
-    'valgrind'
-    'zeal'
-    # Other
-    'dia'
-    'docker'
-    'docker-compose'
-    'xmind'
-    'arandr'
-    'nemo'
-    'otf-ipafont'    # Japanese fonts
-)
-
-
-for pkg in "${install_list[@]}"; do
-    pacman_wrap $pkg
-done
-
+install_editors;
+install_browers;
+install_cmd_tools;
+install_basic_dev_tools;
+install_cpp_dev_tools;
+install_python_dev_tools;
 install_oh_my_zsh;
 install_vbox;
--- utils.sh
+++ utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $(dirname $0)/config.sh
+source $(dirname "$0")/config.sh
 
 RED='\033[0;31m' # Red
 YEL='\033[0;33m' # Yellow
@@ -20,20 +20,20 @@ function logger {
 
 function log_break {
     printf "*%.0s" {1..80} >> log.txt;
-    printf "\n" >>  log.txt;
+    printf "\\n" >>  log.txt;
 }
 
 function pacman_wrap {
     logger "Try to install [$1]" 0;
     log_break;
-    if (pacman -Q $1 &>/dev/null); then
+    if (pacman -Q "$1" &>/dev/null); then
         logger "Already installed [$1]" 0;
 	log_break;
         return
     fi
 
-    pacman -S $1 --noconfirm  &>> log.txt
-    if [ $? == 0 ]; then
+
+    if (pacman -S "$1" --noconfirm  &>> log.txt); then
         logger "Instllation of [$1] OK" 0;
 	log_break;
     else
@@ -45,14 +45,13 @@ function pacman_wrap {
 function yaourt_wrap {
     logger "Try to install [$1]" 0;
     log_break;
-    if (yaourt -Q $1 &>/dev/null); then
+    if (yaourt -Q "$1" &>/dev/null); then
         logger "Already installed [$1]" 0;
 	log_break;
         return
     fi
 
-    yaourt -S $1 --noconfirm  &>> log.txt
-    if [ $? == 0 ]; then
+    if (yaourt -S "$1" --noconfirm  &>> log.txt); then
         logger "Instllation of [$1] OK" 0;
 	log_break;
     else
@@ -61,3 +60,20 @@ function yaourt_wrap {
     fi
 }
 
+function pip_wrap {
+    logger "Try to install python module [$1]" 0;
+    log_break;
+    if (pip list |grep -i "$1" &>/dev/null); then
+        logger "Python module already installed [$1]" 0;
+	log_break;
+        return
+    fi
+
+    if (pip install "$1"  &>> log.txt); then
+        logger "Instllation of [$1] OK" 0;
+	log_break;
+    else
+        logger "Instllation of [$1] ERROR" 2;
+        log_break;
+    fi
+}
