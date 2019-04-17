850d04805bf30344ad3d216fb9f03b79e1c69391
biniu <dawid.binkowski93@gmail.com>
2019-04-17 10:23:59 +0200

    change position of conky widgets

--- own_scripts/calendar.sh
+++ own_scripts/calendar.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cal_var=`date +%_d`;
-cal | sed s/"\(^\|[^0-9]\)$cal_var"'\b'/'\1${font DejaVu Sans Mono:bold:pixelsize=7}${color2}'"$cal_var"'${font}${color}'/ \
+cal | sed s/"\(^\|[^0-9]\)$cal_var"'\b'/'\1${font DejaVu Sans Mono:bold:pixelsize=10}${color2}'"$cal_var"'${font}${color}'/ \
--- own_scripts/run_conky.sh
+++ own_scripts/run_conky.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
-#ps aux|grep conky |xargs kill 
-sleep 1
 
-conky -c ~/.config/conky/conky_calendar       2>/dev/null &
+# ps aux|grep conky|xargs kill
+# sleep 0.5
+
+if [ "$1" = '-d' ]; then
+    echo "Debug mode"
+    echo "Using relative paths"
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    CONF_PATH="$DIR/../conky"
+    export CONF_PATH="$DIR/../conky"
+    export SCRIPT_PATH="$DIR"
+else
+    CONF_PATH="$HOME/.config/conky"
+    export CONF_PATH="$HOME/.config/conky"
+    # TODO(biniu) set for some CONST
+    # expot SCRIPT_PATH="$DIR"
+fi
+
+
+echo "$CONF_PATH"
+
+
+conky -c "$CONF_PATH/conky_calendar"       2>/dev/null &
 sleep 0.1
-conky -c ~/.config/conky/connections          2>/dev/null &
+conky -c "$CONF_PATH/connections"          2>/dev/null &
 sleep 0.1
-conky -c ~/.config/conky/monitoring_panel     2>/dev/null &
+conky -c "$CONF_PATH/monitoring_panel"     2>/dev/null &
 sleep 0.1
-conky -c ~/.config/conky/conky_rss            2>/dev/null &
+conky -c "$CONF_PATH/conky_rss"            2>/dev/null &
 sleep 0.1
-# conky -c ~/.config/conky/conky_todo           2>/dev/null &
+conky -c "$CONF_PATH/conky_spotify"        2>/dev/null &
 sleep 0.1
-conky -c ~/.config/conky/conky_spotify        2>/dev/null &
+conky -c "$CONF_PATH/clock"                2>/dev/null &
 sleep 0.1
-# conky -c ~/.config/conky/conky_taskWarrior    2>/dev/null &
-sleep 0.1
-conky -c ~/.config/conky/clock                2>/dev/null &
-sleep 0.1
+# TODO(biniu) scripts to do
+# gmail
+# weather
+# system logs
+# package to update
+# TODO list
+# calendar agenda
+# NETWORK
+#  ip
+#  speedtest
+# battery
+# other RSS
+# pocket TO READ
+# dropbox usage
